0320460d5ae0ff402f8b199a4ccfdf545b951349
Hamza Ali <hamzaali528@gmail.com>
2024-02-21 00:49:30 +0500

    Admin work

--- cypress/e2e/1-getting-started/login.cy.js
+++ cypress/e2e/1-getting-started/login.cy.js
@@ -1,17 +1,21 @@
+import AdminPage from "../../PageObjectModel/Admin";
 import BasePage from "../../PageObjectModel/BasePage";
 import LoginPage from "../../PageObjectModel/LoginPage";
 describe('Login : Test Suite', ()=> {
 
     const bs = new BasePage
     const ln = new LoginPage
+    const admin = new AdminPage
 
 beforeEach(() => {
     bs.Visit()
 })
 
-it('Verify Valid Login', () => {
+it.only('Verify Valid Login', () => {
     bs.Wait_for(5000)
     ln.Login_With_Valid_User()
+    admin.Admin_Add_User();
+
 });
 
 it('Verify InValid Login', () => {
